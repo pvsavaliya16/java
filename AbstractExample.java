7483d44aa9d964685679b5adb0199194bcd61f2a
pvsavaliya16 <38027470+pvsavaliya16@users.noreply.github.com>
2019-01-29 09:31:32 +0530

    Update AbstractExample.java

--- AbstractExample.java
+++ AbstractExample.java
@@ -5,23 +5,49 @@
  */
 package parth;
 
-abstract class AbstractClass {
-    private int a;
-    private int b;
-
-    public AbstractClass(int a, int b) {
-        this.a = a;
-        this.b = b;
-        
-    }
-    
+abstract class Polyhedron {
+    int length;
+    Polyhedron(int length) {
+        this.length = length;
+        System.out.println("ABstract class constructor called");
+    } 
+    abstract void calculateVolume(); 
 }
 
-public class AbstractExample extends AbstractClass {
-    int c; 
-    public AbstractExample(int a, int b, int c) {
-        super(a, b);
-        this.c = c;
+class Cube extends Polyhedron {
+    Cube(int length) {
+        super(length);
+        System.out.println("Cube constructor called");
     }
-    
+    @Override
+    void calculateVolume() {
+        System.out.println("Volume = " + (Math.pow(this.length, 3)));
+    }
 }
+
+class Cuboid extends Polyhedron {
+    private int breadth, height;
+
+    Cuboid(int length, int breadth, int height) {
+        super(length);
+        this.breadth = breadth;
+        this.height = height;
+        System.out.println("Cuboid constructor called");
+    }
+    @Override
+    void calculateVolume() {
+        System.out.println("Volume = " + (this.length * this.breadth * this.height));
+    }
+}
+
+
+class AbstractExample {
+    public static void main(String[] args) {
+        Polyhedron p1 = new Cube(10);
+        Polyhedron p2 = new Cuboid(2, 3, 5);
+        p1.calculateVolume();
+        p2.calculateVolume();
+    }
+}
+
+
